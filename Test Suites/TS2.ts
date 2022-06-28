e3b537fb78595690bf880d0d94c31e42274f54bb
shreyansh.r <shreyansh.r@LSQLAP-7717.lsq.net>
2022-06-28 11:47:55 +0530

    second commit

---
+++ Test Suites/TS2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0eea034b-179b-4d21-8f9a-9154919773e8</testSuiteGuid>
+</TestSuiteEntity>
